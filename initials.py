07e006943164c2ae484a41e4cf5e4fd6a021c677
Calvin-Dengyimah <2425403761@live.gctu.edu.gh>
2026-05-30 22:59:34 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+#File initials.py
+#Description:Print out my initials.
+#Assignment:1
+#
+#Name:Calvin Obubah Dengyimah
+#Student ID: 2425403761
+# Email:2425403761@live.gctu.edu.gh
+#Grader:Augustus Buckman
+#
+#On my honor, Calvin Obubah Dengyimah, this programming assignment is my own work
+#and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+        # Print my initials in large block letters
+
+        print()
+        print("...COD")
+        print()
 
 
+        print("...CCCCCCCC...OOOOOOOO...DDDDDDDD...")
+        print("...CC.........OO....OO...DD.....DD..")
+        print("...CC.........OO....OO...DD......DD.")
+        print("...CC.........OO....OO...DD......DD.")
+        print("...CC.........OO....OO...DD......DD.")
+        print("...CC.........OO....OO...DD......DD.")
+        print("...CC.........OO....OO...DD......DD.")
+        print("...CC.........OO....OO...DD......DD.")
+        print("...CC.........OO....OO...DD.....DD..")
+        print("...CCCCCCCC...OOOOOOOO...DDDDDDDD...")
+
+        print()
 
 main()
